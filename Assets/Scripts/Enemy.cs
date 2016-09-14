7d5b62c24b0abfd6cb6288ef8e70cc30fa812840
root <scrumgames@gmail.com>
2016-09-14 00:20:12 -0300

    commit inicial

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float vida = 100;
+    bool chamouMorte = false;
+
+    void Update()
+    {
+        if (vida <= 0)
+        {
+            vida = 0;
+            if (chamouMorte == false)
+            {
+                chamouMorte = true;
+                StartCoroutine("Morrer");
+            }
+        }
+    }
+
+    IEnumerator Morrer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
