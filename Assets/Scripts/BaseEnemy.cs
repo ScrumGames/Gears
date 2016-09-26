ded81ec013f147178dfe3524018f91e2635de902
Tony Salto <tonysaltodev@gmail.com>
2016-09-25 22:58:48 -0300

    Adicionando organização de pastas tradicional, criando spaw do player, iniciando spaw do inimigo, faltando terminar a parte de IA de movimentação

---
+++ Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgentController))]
+public class BaseEnemy : MonoBehaviour 
+{
+	private NavMeshAgentController navMeshAgentController;
+
+	private void Awake()
+	{
+		navMeshAgentController = GetComponent<NavMeshAgentController> ();
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/Gameplay/EnemySpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour 
+{
+	[SerializeField] private float _spawNewEnemyInterval = 5.0f;
+	[SerializeField] private int _maxEnemies = 10;
+	[SerializeField] private BaseEnemy _enemyPrefab;
+
+	private List<BaseEnemy> _aliveEnemies = new List<BaseEnemy>();
+	private bool _alive;
+
+	private void Start()
+	{
+		StartCoroutine (SpawEnemyCoroutine ());
+	}
+
+	private IEnumerator SpawEnemyCoroutine()
+	{
+		_alive = true;
+
+		while (_alive) 
+		{
+			if (CanSpawnMoreEnemies())
+				SpawNewEnemy ();
+
+			yield return new WaitForSeconds (_spawNewEnemyInterval);
+		}
+
+	}
+
+	private bool CanSpawnMoreEnemies()
+	{
+		if (_aliveEnemies.Count < _maxEnemies)
+			return true;
+
+		return false;
+	}
+
+	private void SpawNewEnemy()
+	{
+		BaseEnemy enemyClone = Instantiate (_enemyPrefab);
+		enemyClone.transform.SetParent (this.transform);
+		enemyClone.transform.localPosition = Vector3.zero;
+
+		_aliveEnemies.Add (enemyClone);
+
+
+	}
+
+
+}
