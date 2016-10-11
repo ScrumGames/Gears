b717b30aab0d1d8ba98b9b5146ac555fc0c658b8
Unknown <thiagocgp@gmail.com>
2016-10-10 21:00:39 -0300

    Implementado interações iniciais

---
+++ Assets/FPSCharacter/Scripts/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+    private List<GameObject> inventoryObjects;
+    private int inventorySize = 6;
+
+    void Start()
+    {
+        inventoryObjects = new List<GameObject>();
+    }
+
+    public GameObject RemoveObjectInventory()
+    {
+        GameObject obj = inventoryObjects[0];
+        inventoryObjects.RemoveAt(0);
+        return obj;
+    }
+
+    public bool AddObjectInventory(GameObject obj)
+    {
+        if (!isFull())
+        {
+            inventoryObjects.Add(obj);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool isFull()
+    {
+        if (inventoryObjects.Count < inventorySize)
+        {
+            return false;
+        }
+        else
+            return true;
+    }
+
+    public bool isEmpty()
+    {
+        if (inventoryObjects.Count == 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+}
